56b15818f16c3ff49a6a6595ed8015734a426432
boubacarDiatta <bs.diatta@digiafrika.com>
2025-09-17 12:05:31 +0000

    feat: structure de base du dashboard

--- app/components/layout/Layout.tsx
+++ app/components/layout/Layout.tsx
@@ -2,16 +2,20 @@ import { Outlet } from "react-router";
 
 export default function Layout() {
   return (
-    <main>
-      <header></header>
-      <NavMenu />
-      <div>
-        <Outlet />
+    <main className="w-full h-screen ">
+      <header className="w-full h-[80px] bg-white shadow "></header>
+      <div className=" w-full h-[calc(100vh-80px)]  flex  gap-2">
+        <NavMenu />
+        <div>
+          <Outlet />
+        </div>
       </div>
     </main>
   );
 }
 
 const NavMenu = () => {
-  return <nav>navigation par profile</nav>;
+  return (
+    <nav className="w-[320px] h-full bg-gray-200 shadow">navigation par profile</nav>
+  );
 };
